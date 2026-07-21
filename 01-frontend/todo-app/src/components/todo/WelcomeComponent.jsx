0cba6f57c23b3415326059a6ec916a8223b1b9e1
Abhishek Bhatt <abhishekbhatt393@gmail.com>
2026-07-22 00:31:33 +0530

    1st section of dashboard added

--- 01-frontend/todo-app/src/components/todo/WelcomeComponent.jsx
+++ 01-frontend/todo-app/src/components/todo/WelcomeComponent.jsx
@@ -1,49 +1,87 @@
-import { Link,useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useState } from "react";
 import { retrieveHelloWorldPathVar } from "./api/HelloWorldApiService";
+import "./WelcomeComponent.css";
+import {
+  LuListTodo,
+  LuClock3,
+  LuBadgeCheck,
+  LuChartColumn,
+} from "react-icons/lu";
 import { useAuth } from "./security/AuthContext";
 
 export default function WelcomeComponent() {
-    const {username} =useParams();
-    const[message,setMessage]=useState(null)
-    const authContext=useAuth()
+  const { username } = useParams();
+  const [message, setMessage] = useState(null);
+  const authContext = useAuth();
 
-    // console.log(username)
-    function callHelloWorld(){
-        // axios.get("http://localhost:8080/hello-world")
-        // .then((response)=>ShowSucess(response))
-        // .catch((error)=>ShowError(error))
-        // .finally(()=>console.log("clean up"))
+  // console.log(username)
+  function callHelloWorld() {
+    // axios.get("http://localhost:8080/hello-world")
+    // .then((response)=>ShowSucess(response))
+    // .catch((error)=>ShowError(error))
+    // .finally(()=>console.log("clean up"))
 
-        // retrieveHelloWorld()
-        // .then((response)=>ShowSucess(response))
-        // .catch((error)=>ShowError(error))
-        // .finally(()=>console.log("clean up"))
-        
-        retrieveHelloWorldPathVar("Abhishek",authContext.token)
-        .then((response)=>ShowSucess(response))
-        .catch((error)=>ShowError(error))
-        .finally(()=>console.log("clean up"))
+    // retrieveHelloWorld()
+    // .then((response)=>ShowSucess(response))
+    // .catch((error)=>ShowError(error))
+    // .finally(()=>console.log("clean up"))
 
-    }   
-    
-    function ShowSucess(response){
-        console.log(response)
-        setMessage(response.data.message)
-    }
-    function ShowError(error){
-        console.log(error)
-    }
+    retrieveHelloWorldPathVar("Abhishek", authContext.token)
+      .then((response) => ShowSucess(response))
+      .catch((error) => ShowError(error))
+      .finally(() => console.log("clean up"));
+  }
+
+  function ShowSucess(response) {
+    console.log(response);
+    setMessage(response.data.message);
+  }
+  function ShowError(error) {
+    console.log(error);
+  }
   return (
-     <div className="WelcomeComponent">
-    <h1>Welcome {username}!</h1>
-    <div>
-        Mange your todo. - <Link to="/todos">Go Here</Link>
+    <div className="dashboard-container">
+      {/* Greeting */}
+      <div className="dashboard-header">
+        <h1>Good Evening 👋</h1>
+        <p>Welcome back, {username}</p>
+      </div>
+
+      {/* Statistics */}
+      <div className="stats-container">
+        <div className="stat-card">
+          <LuListTodo className="stat-icon total-icon" />
+
+          <h2>12</h2>
+
+          <span>Total Tasks</span>
+        </div>
+
+        <div className="stat-card">
+          <LuClock3 className="stat-icon pending-icon" />
+
+          <h2>5</h2>
+
+          <span>Pending</span>
+        </div>
+
+        <div className="stat-card">
+          <LuBadgeCheck className="stat-icon completed-icon" />
+
+          <h2>7</h2>
+
+          <span>Completed</span>
+        </div>
+
+        <div className="stat-card">
+          <LuChartColumn className="stat-icon progress-icon" />
+
+          <h2>58%</h2>
+
+          <span>Progress</span>
+        </div>
+      </div>
     </div>
-    <div>
-        <button className="btn btn-success m-5" onClick={callHelloWorld}>Call Hello World</button>
-    </div>
-    <div className="text-info">{message}</div>
-    </div>
-  )
+  );
 }
